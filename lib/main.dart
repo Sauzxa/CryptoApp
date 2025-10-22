ac9a9b9858bb3da7ea8cc49497f4235b48ae23e9
sauzxa <feraouf91@gmail.com>
2025-10-22 04:50:29 +0100

    frinaha m3a classement f suivi page

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-//
+//f
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
